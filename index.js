acd23d413a73dbd5f46afd9d5bd1f570c0e4de5d
Diego Marques <diegomarques@iMac.local>
2019-09-13 08:57:13 -0300

    chore: Modulo 01 - Comentários de desenvolvimento

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+////Iniciar o express
 const express = require("express");
 
+////Setar a varíavel com a função do express
 const server = express();
 
+/// Utilizar o Json no express
 server.use(express.json());
 
+/// Constante de usuários
 const users = ["Diego", "Robson", "Wilson"];
 
+/// Conceito de Middleware Global
 server.use((req, res, next) => {
   console.time("Request");
   console.log(`Método: ${req.method}; URL: ${req.url}`);
@@ -13,6 +18,7 @@ server.use((req, res, next) => {
   console.timeEnd("Request");
 });
 
+//Conceito de Middleware Local com função
 function checkUserExists(req, res, next) {
   if (!req.body.name) {
     return res.status(400).json({ error: "User name is required" });
@@ -31,16 +37,20 @@ function checkUserInArray(req, res, next) {
 
   return next();
 }
+
+///// Rota de listagem de usuários
 server.get("/users", (req, res) => {
   return res.json(users);
 });
 
+//// Rotas de Listagem de um usuário, aplicando o middleware pra ver se existe
 server.get("/users/:index", checkUserInArray, (req, res) => {
   const { index } = req.params;
 
   return res.json(req.user);
 });
 
+//// Rota de Criação de usuário com middleware verificando se não está vazio
 server.post("/users", checkUserExists, (req, res) => {
   const { name } = req.body;
 
@@ -49,6 +59,7 @@ server.post("/users", checkUserExists, (req, res) => {
   return res.json(users);
 });
 
+//// Rota de Alteração de usuário com duas verificações
 server.put("/users/:index", checkUserInArray, checkUserExists, (req, res) => {
   const { index } = req.params;
   const { name } = req.body;
@@ -57,10 +68,12 @@ server.put("/users/:index", checkUserInArray, checkUserExists, (req, res) => {
   return res.json(users);
 });
 
+/// Rota de delete de usuário com verificação de index
 server.delete("/users/:index", checkUserInArray, (req, res) => {
   const { index } = req.params;
   users.splice(index, 1);
   return res.send();
 });
 
+/// Porta onde o servidor vai responder
 server.listen(3000);
